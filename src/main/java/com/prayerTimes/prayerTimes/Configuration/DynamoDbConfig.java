4fcbedb9c580444a445f26507b009809dedad87f
unknown <mali30@fordham.edu>
2022-08-24 22:03:06 -0400

    adding DynamoDB Configuration

---
+++ src/main/java/com/prayerTimes/prayerTimes/Configuration/DynamoDbConfig.java
@@ -0,0 +1,43 @@
+package com.prayerTimes.prayerTimes.Configuration;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Value("${amazon.aws.accesskey}")
+    private String amazonAccessKey;
+
+    @Value("${amazon.aws.secretkey}")
+    private String amazonSecretKey;
+
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
+    @Bean
+    DynamoDBMapper dynamoDBMapper(){
+        return new DynamoDBMapper(amazonDynamoDB());
+    }
+
+    private AmazonDynamoDB amazonDynamoDB(){
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint,""))
+                .withCredentials(amazonDynamoDBCredentials()).build();
+
+    }
+
+    private AWSCredentialsProvider amazonDynamoDBCredentials(){
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(amazonAccessKey , amazonSecretKey));
+    }
+
+}
